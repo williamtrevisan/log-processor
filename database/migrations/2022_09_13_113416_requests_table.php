3fce3b3164785bd38242ef0811fda6b652a482e0
William Trevisan <williamtrev@hotmail.com>
2022-09-13 12:00:03 -0300

    feat: create requests table migration

---
+++ database/migrations/2022_09_13_113416_requests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->uuid('consumer_id');
+            $table->uuid('service_id');
+            $table->string('service_name');
+            $table->string('method');
+            $table->string('uniform_resource_identifier');
+            $table->string('uniform_resource_locator');
+            $table->smallInteger('size');
+            $table->smallInteger('response_status');
+            $table->smallInteger('proxy_latency');
+            $table->smallInteger('kong_latency');
+            $table->smallInteger('request_latency');
+            $table->ipAddress('client_ip');
+            $table->timestamp('started_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
